f08940731e439d81822b663cacba08d5c981ceb9
Priti Jadhav <pritijadhav856@gmail.com>
2024-02-12 16:58:14 +0530

    Create 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello")
+
+//we are using nodejs, not browser
+
+let name="Priti"
+let age=21
+let isLoggedIn=false
+
+//Datatypes:-
+/*primitive*/
+//number=>2to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof "Priti");//string
+console.log(typeof undefined);//undefined
+console.log(typeof null)//object
