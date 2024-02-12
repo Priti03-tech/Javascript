531df2187c6594f0fbd7857672b4c72fdff2d8b6
Priti Jadhav <pritijadhav856@gmail.com>
2024-02-12 17:50:59 +0530

    Create 04_operations.js

---
+++ 04_operations.js
@@ -0,0 +1,61 @@
+//let age="21abc"
+let age=null
+
+// console.log(typeof age);
+// console.log(typeof(age));
+
+let valueInNumber=Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"21"=>21
+//"21abc"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true;0=>false
+//""=>false
+//"priti"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//Operations
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%5);
+// console.log(2**3);
+
+let str1="Hello"
+let str2=" Priti"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log("1"+"2");
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+//Link to study
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
