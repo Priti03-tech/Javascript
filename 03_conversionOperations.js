0a2ffb3c264730b8d835352650927d1f9b51d83c
Priti Jadhav <pritijadhav856@gmail.com>
2024-02-12 17:19:10 +0530

    Create 03_conversionOperations.js

---
+++ 03_conversionOperations.js
@@ -0,0 +1,27 @@
+//let age="21abc"
+let age=null
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let valueInNumber=Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"21"=>21
+//"21abc"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true;0=>false
+//""=>false
+//"priti"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
