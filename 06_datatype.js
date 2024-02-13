aec12e16e6b74b64ed74c6fff658d52e25eb331f
Priti Jadhav <pritijadhav856@gmail.com>
2024-02-13 17:22:46 +0530

    summary

---
+++ 06_datatype.js
@@ -0,0 +1,37 @@
+// #Primitives datatypes
+
+//It having 7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTEMP=null
+
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber=5476755872362n
+
+//Reference type(non primitive)
+
+//Array,objects,functions
+
+const heroes=["shaktiman","nagraj","doga"];
+let myObj={
+    name:"Priti",
+    age:21,
+}
+
+const myFunction=function(){
+
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
+
