a38673c32428e1d815d4034472b56f8dfb7a90d3
Priti Jadhav <pritijadhav856@gmail.com>
2024-02-13 16:57:34 +0530

    comparison

---
+++ 05_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+
+
+// Strict check
+//===:-It not only checks the values bt also check the datatypes strictly
+
+console.log("2"===2);
