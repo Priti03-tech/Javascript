4db1fe27dd86d02b913682703e7626a37677d901
Priti Jadhav <pritijadhav856@gmail.com>
2024-03-04 18:32:42 +0530

    math_and_num

---
+++ 09_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score=400
+
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber=145.8899
+
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds=100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++MATHS++++++++++++++++
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random() * ( max-min+1))+min);
