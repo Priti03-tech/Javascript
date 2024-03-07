2aacccf8110b5046a99cd1e6f24c7c5a21973249
Priti Jadhav <pritijadhav856@gmail.com>
2024-03-07 15:16:44 +0530

    array2

--- 01_basics/01_array.js
+++ 01_basics/01_array.js
@@ -8,20 +8,20 @@ console.log(myArr[0]);
 
 //Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
-//console.log(myArr);
+myArr.unshift(9)
+myArr.shift()
+console.log(myArr);
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
 
-// const newArr=myArr.join()
-// console.log(myArr);
-// console.log(newArr);
+const newArr=myArr.join()
+console.log(myArr);
+console.log(newArr);
 
 //slice,splice
 
