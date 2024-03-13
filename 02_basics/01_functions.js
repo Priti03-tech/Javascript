e9e96a88d70dcf1b074fc0aac25ac8eaeec46e28
Priti Jadhav <pritijadhav856@gmail.com>
2024-03-13 17:35:35 +0530

    functions

---
+++ 02_basics/01_functions.js
@@ -0,0 +1,25 @@
+//Functions
+
+// function addTwoNumbers(number1,number2)
+// {
+//     console.log(number1+number2);
+// }
+function addTwoNumbers(number1,number2)
+{
+    let result=number1+number2
+    return result
+    }
+const result=addTwoNumbers(4,8) 
+//console.log("Result:",result); 
+
+function LoginUser(username)
+{
+    if(username===undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return`${username} just logged in`
+}
+
+console.log(LoginUser("Priti"));
+console.log(LoginUser());
