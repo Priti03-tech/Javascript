e8b40b8c3c84844498d6867604789c5d41cf5be9
Priti Jadhav <pritijadhav856@gmail.com>
2024-03-17 23:06:48 +0530

    scopes

--- 02_basics/03_scopes.js
+++ 02_basics/03_scopes.js
@@ -3,6 +3,8 @@
 // let a=10
 // const b=20
 // var c=30
+let a=300
+
 if(true){
     let a=10
     const b=20
@@ -11,4 +13,45 @@ if(true){
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+//console.log(c);
+
+function one(){
+    const username="Priti"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+   // console.log(website);
+
+    two()
+
+}
+
+//one()
+
+if(true){
+    const username="Priti"
+    if(username==="Priti"){
+        const website=" youtube"
+       // console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+
+//+++++++++++Interesting concept+++++++++++++
+
+function addone(num){
+    return num+1
+}
+
+addone(5)
+//addTwo(5)
+const addTwo=function(num){
+    return num+2
+}
+
+console.log(addTwo(5))
